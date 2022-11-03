ea0324ed2afe7dfa3e937bcd92f8a9287570c407
Marius Hambro Bovbjerg <marius@hambro.eu>
2022-11-04 00:08:06 +0100

    yo

--- DataSource/Program.cs
+++ DataSource/Program.cs
@@ -29,7 +29,7 @@ class Program
 
             foreach (var guid in _guids)
             { 
-                var newMoisture = (int) Math.Clamp(Math.Round(guid.Value + ((double)random.Next(-100, 100) / 100)), 0, 100);
+                var newMoisture = Math.Clamp(guid.Value + random.Next(-1, 1), 0, 100);
                 
                 _guids[guid.Key] = newMoisture;
                 
