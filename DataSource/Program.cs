95be5fc08a370353d81adb01a8bc91a3c0cf9a64
Marius Hambro Bovbjerg <marius@hambro.eu>
2022-11-03 21:56:30 +0100

    Datasource program smol program

---
+++ DataSource/Program.cs
@@ -0,0 +1,35 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using Newtonsoft.Json;
+
+namespace DataSource;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var mqttFactory = new MqttFactory();
+
+        using (var mqttClient = mqttFactory.CreateMqttClient())
+        {
+            var mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer(Environment.GetEnvironmentVariable("MQTT_Broker") ?? "localhost", 1883)
+                .Build();
+
+            await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+            var json = JsonConvert.SerializeObject(new { clientid = Guid.NewGuid(), timestamp = DateTime.Now });
+
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("water")
+                .WithPayload(json)
+                .Build();
+
+            await mqttClient.PublishAsync(message, CancellationToken.None);
+
+            await mqttClient.DisconnectAsync();
+
+            Console.WriteLine("MQTT application message is published.");
+        }
+    }
+}
